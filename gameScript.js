b24b44fab974b6eef0395f57b17ed7534afc12b1
RSimer <65471088+RSimer@users.noreply.github.com>
2021-05-24 14:40:01 -0700

    just adding some stuff

--- gameScript.js
+++ gameScript.js
@@ -6,64 +6,76 @@ var mainEl = document.getElementById("main");
 
 var secondsLeft = 10;
 
+// start of the time function 
+
+
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " scs left";
+        var timerInterval = setInterval(function() {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft + " scs left";
 
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to stop the test and send to scores screen
-      stop();
-    }
+            if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to stop the test and send to scores screen
+            stop();
+            }
 
-  }, 1000);
+        }, 1000);
 }
 
 // stop the test altogether
 function stop() {
-  timeEl.textContent = " ";
-
+            timeEl.textContent = " ";
+            var btn = $(".question");
+            btn[0].onclick = false;
 }
 
 setTime();
 // end timer section
 
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // begin quiz section randomization and maybe how to do it honestly idk
 
 // variables and and wtf am I doing
 var quiz = [];
-quiz[0] = new Question();
-quiz[1] = new Question();
-quiz[2] = new Question();
-quiz[3] = new Question();
-quiz[4] = new Question();
-quiz[5] = new Question();
-quiz[6] = new Question();
-quiz[7] = new Question();
-quiz[8] = new Question();
-quiz[9] = new Question();
-quiz[10] = new Question();
+            quiz[0] = new Question();
+            quiz[1] = new Question();
+            quiz[2] = new Question();
+            quiz[3] = new Question();
+            quiz[4] = new Question();
+            quiz[5] = new Question();
+            quiz[6] = new Question();
+            quiz[7] = new Question();
+            quiz[8] = new Question();
+            quiz[9] = new Question();
+            quiz[10] = new Question();
 
 var randomQuestion;
 var answers = [];
 var currentScore = 0;
-shuffle(quiz);
-console.log(quiz);
+        shuffle(quiz);
+        console.log(quiz);
 
+// this will suffle the quiz array... yeah
 function shuffle(quiz) {
-    var currentIndex = quiz.length, temp, randomIndex;
-    while (0 !== currentIndex) {
+        var currentIndex = quiz.length, temp, randomIndex;
+        while (0 !== currentIndex) {
 
 
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex = 0;
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex = 0;
 
-        temp = quiz[currentIndex];
-        quiz[currentIndex] = quiz [randomIndex];
-        quiz[randomIndex] = temp;
+            temp = quiz[currentIndex];
+            quiz[currentIndex] = quiz [randomIndex];
+            quiz[randomIndex] = temp;
 
     }
 }
+// end randomization/shuffle section
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
--- readme.md
+++ readme.md
@@ -9,3 +9,5 @@
 https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 
 https://codepen.io/savant/pen/gbaveO?editors=1010
+
+https://stackoverflow.com/questions/38157927/prevent-onclick-from-firing
