99705ff5b5cbbdcdc2112d988572017720c5aac9
RSimer <65471088+RSimer@users.noreply.github.com>
2021-05-26 21:33:41 -0700

    gotta love finding new answers that I like better'

--- gameScript.js
+++ gameScript.js
@@ -1,10 +1,9 @@
 // gonna start with time becuase i can ctrl c and ctrl v that 
 
 
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+var timeEl = document.querySelector("#timer");
 
-var secondsLeft = 10;
+var secondsLeft = 15;
 
 // start of the time function 
 
@@ -19,6 +18,7 @@ function setTime() {
             clearInterval(timerInterval);
             // Calls function to stop the test and send to scores screen
             stop();
+            endTime();
             }
 
         }, 1000);
@@ -27,11 +27,15 @@ function setTime() {
 // stop the test altogether
 function stop() {
             timeEl.textContent = " ";
-            var btn = $(".question");
-            btn[0].onclick = false;
+           
 }
 
-setTime();
+
+function endTime(){
+        if (time === 0){
+                document.querySelector("#").textContent = "";
+        }
+}
 // end timer section
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -39,43 +43,21 @@ setTime();
 // begin quiz section randomization and maybe how to do it honestly idk
 
 // variables and and wtf am I doing
-var quiz = [];
-            quiz[0] = new Question();
-            quiz[1] = new Question();
-            quiz[2] = new Question();
-            quiz[3] = new Question();
-            quiz[4] = new Question();
-            quiz[5] = new Question();
-            quiz[6] = new Question();
-            quiz[7] = new Question();
-            quiz[8] = new Question();
-            quiz[9] = new Question();
-            quiz[10] = new Question();
+var quiz = [
 
-var randomQuestion;
-var answers = [];
-var currentScore = 0;
-        shuffle(quiz);
-        console.log(quiz);
+        {
+                title: "",
+                choices: [],
+                answer: ""
 
-// this will suffle the quiz array... yeah
-function shuffle(quiz) {
-        var currentIndex = quiz.length, temp, randomIndex;
-        while (0 !== currentIndex) {
+        },
+];
 
-
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex = 0;
-
-            temp = quiz[currentIndex];
-            quiz[currentIndex] = quiz [randomIndex];
-            quiz[randomIndex] = temp;
-
-    }
-}
 // end randomization/shuffle section
-
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// Start of IF answer is right or wrong
 
+
+// inner.html = quiz[0]; allowance of moving to different question
